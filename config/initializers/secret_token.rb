ef023fe3b09137017e6d1a1c95e0613299dd2e8c
MATC Student <student@MATC1234.(none)>
2012-10-06 16:54:18 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '8ba8c4018755c5ccaaeae277d947da05e5e528f02d7a07e0696433dbde2980739fc31e07899b110bb9190ea346a61d089feac8c2117c9e017bac1497e75713d1'
