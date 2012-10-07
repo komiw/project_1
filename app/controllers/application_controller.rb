5a3f94bab15dd264cea2707fa37d1fc8f0593b8f
MATC Student <student@MATC1234.(none)>
2012-10-07 14:16:43 -0500

    Final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  validates :url, :presence => true
+  validates :name, :presence => true, :length => { :minimum => 3, :maximum => 100 }
 end
